24d133dbb24c0818f701e480c167743483a2c9dc
prescottbreeden <prescott.breeden@gmail.com>
2018-11-02 01:08:05 -0700

    first exercise

--- fundamentals/area_rectangle/main.c
+++ fundamentals/area_rectangle/main.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  main.c
  *
- *    Description:  
+ *    Description:  display the perimeter and area of a rectangle
  *
  *        Version:  1.0
  *        Created:  11/01/2018 03:20:06 PM
@@ -16,4 +16,18 @@
  * =====================================================================================
  */
 #include <stdlib.h>
+#include <stdio.h>
 
+int main(int argc, char *argv[]) {
+
+  int width = *argv[1] - '0';
+  int height = *argv[2] - '0';
+  int perimeter;
+  int area;
+
+  perimeter = width*2 + height*2;
+  area = width * height;
+  printf("Width: %i, Height: %i, Perimeter: %i, Area: %i\n", 
+          width, height, perimeter, area);
+  return 0;
+}
